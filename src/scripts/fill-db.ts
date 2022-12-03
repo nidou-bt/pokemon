c57255e63e0144661a380d0e6b1528630d716a5e
Nidhal Ben Taarit <nidhal.bentaarit@korsgy.com>
2022-12-03 17:39:39 +0100

    add node-ts

---
+++ src/scripts/fill-db.ts
@@ -0,0 +1,24 @@
+import { PokemonClient } from "pokenode-ts";
+import { prisma } from "../server/utils/prisma";
+
+const doBackfill = async () => {
+  const pokeApi = new PokemonClient();
+
+  const allPokemon = await pokeApi.listPokemons(0, 493);
+
+  const formattedPokemon = allPokemon.results.map((p, index) => ({
+    // name: (p as { name: string }).name,
+    name: p.name,
+    spriteUrl: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+      index + 1
+    }.png`,
+  }));
+
+  const creation = await prisma.pokemon.createMany({
+    data: formattedPokemon,
+  });
+
+  console.log("Creation?", creation);
+};
+
+doBackfill();
